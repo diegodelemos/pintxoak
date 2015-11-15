c6d717f320d8d9313d5f9db6caa9b3ce2e323aba
Eliot <thevlanko@gmail.com>
2015-11-15 13:15:31 +0100

    Multilang file added.

---
+++ core/Lang.php
@@ -0,0 +1,15 @@
+<?php
+  $lang = array(
+    'en' => array(
+      'Please, sign in' => 'Please, sign in',
+      'User name' => 'User name',
+      'Password' => 'Password'
+      'Sign in' => 'Sign in'
+    );
+    'es' => array(
+      'Please, sign in' => 'Por favor, ingrese',
+      'User name' => 'Nombre de usuario',
+      'Password' => 'Contraseña'
+      'Sign in' => 'Ingresar'
+    );
+  );
