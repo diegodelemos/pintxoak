54a5bca40e952e38283922d4b53f1c7f6249db95
diegodelemos <diegomonforte1@gmail.com>
2015-11-24 14:00:45 +0100

    Last change first iteration I promise. Duplicate sql sentence in populate sql

--- sql/populate.sql
+++ sql/populate.sql
@@ -5,9 +5,6 @@ INSERT INTO ORGANIZER (o_username) VALUES ("diego");
 INSERT INTO ORGANIZER (o_username) VALUES ("eliot");
 
 /* JURY */
-INSERT INTO USER (username, password) VALUES ("juanjurado", "e8dc8ccd5e5f9e3a54f07350ce8a2d3d");
-INSERT INTO USER (username, password) VALUES ("pepejurado", "e8dc8ccd5e5f9e3a54f07350ce8a2d3d");
-INSERT INTO USER (username, password) VALUES ("luisjurado", "e8dc8ccd5e5f9e3a54f07350ce8a2d3d");
 INSERT INTO JUDGE (j_username, j_name, j_profession, j_photo)
        	    	  VALUES ("juanjurado", "Juan Castillo Real",
 		  "Cocinero", "juancastilloreal.png");
